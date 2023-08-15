d987fb845b65f68a0625a163b8e6a8e09dd3192c
Hammad <hmdshfq@outlook.com>
2023-08-15 22:24:01 +0200

    add mongodb and mongoose

--- myApp.js
+++ myApp.js
@@ -1,55 +1,60 @@
 require('dotenv').config();
-
+let mongoose = require('mongoose');
 
 let Person;
 
 const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+    done(null /*, data*/);
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+    done(null /*, data*/);
 };
 
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+    done(null /*, data*/);
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+    done(null /*, data*/);
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+    done(null /*, data*/);
 };
 
 const findEditThenSave = (personId, done) => {
-  const foodToAdd = "hamburger";
+    const foodToAdd = 'hamburger';
 
-  done(null /*, data*/);
+    done(null /*, data*/);
 };
 
 const findAndUpdate = (personName, done) => {
-  const ageToSet = 20;
+    const ageToSet = 20;
 
-  done(null /*, data*/);
+    done(null /*, data*/);
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+    done(null /*, data*/);
 };
 
 const removeManyPeople = (done) => {
-  const nameToRemove = "Mary";
+    const nameToRemove = 'Mary';
 
-  done(null /*, data*/);
+    done(null /*, data*/);
 };
 
 const queryChain = (done) => {
-  const foodToSearch = "burrito";
+    const foodToSearch = 'burrito';
 
-  done(null /*, data*/);
+    done(null /*, data*/);
 };
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 /** **Well Done !!**
 /* You completed these challenges, let's go celebrate !
